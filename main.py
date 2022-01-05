07f7a6760ee5bcd4452da0b5a7ee2f28f6e61cef
Naachiket Pant <naachiketpant88@gmail.com>
2022-01-05 14:58:45 +0530

    Connected to mysql and added dictionaries for tables

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ email: naachiketpant88@gmail.com
 import mysql.connector
 from mysql.connector import Error
 
+
+developer = {'developer_id': 0, 'name' : '', 'email' : '', 'project_id': 0}
+manager = {'manager_id' : 0, 'manager_name' : ''}
+project = {'project_id' : 0, 'name' : '', 'description' : '', 'date_created' : ''}
+issue = {'issue_id' : 0, 'project_id' : 0, 'title' : '', 'description' : '', 'priority' : '', 'created_on' : '', 'due_date' : '', 'assigned_to_developer_id' : int}
+
+
+
+
 try:
-    connection = mysql.connector.connect(host = 'localhost', database = 'bug_tracker', user = 'naachiket', password = '')
+    connection = mysql.connector.connect(host = 'localhost', database = 'bug_tracker',
+                 user = 'naachiket', password = '')
 
     if connection.is_connected():
         print("Connected")
@@ -16,8 +26,9 @@ try:
 
         #query
         query = """insert into project values(
-                    1, 'Omega', 'Nice Project',
-                    '2090-06-26')"""
+                   2, 'Project2', 'This is project 2',
+                   '2090-06-26')"""
+
         cursor.execute(query)
         connection.commit()
 
